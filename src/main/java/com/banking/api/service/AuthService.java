e0ec1d0ee4b7cbac4d60a374894e0cb8d7089d72
Seyad <seyadabdurraheem@gmail.com>
2026-03-09 13:02:52 +0530

    created authService

--- src/main/java/com/banking/api/service/AuthService.java
+++ src/main/java/com/banking/api/service/AuthService.java
@@ -13,4 +13,17 @@ public class AuthService {
 		this.userrepository = new UserRepository();
 	}
 	
+	public User authenticate(String username, String plainTextPassword) {
+		User user = userrepository.findByUsername(username);
+		String hashedPassword = BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+		
+		if(user == null) {
+			return null;
+		}
+		
+		if( hashedPassword == user.getpasswordhash() ) {
+			return user;
+		}
+		return null;
+	}
 }
