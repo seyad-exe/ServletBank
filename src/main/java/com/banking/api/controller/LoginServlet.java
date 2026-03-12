0116409ddb33cf95e5a1bdb270c91764850f4477
Seyad <seyadabdurraheem@gmail.com>
2026-03-12 10:56:03 +0530

    created login servlet

--- src/main/java/com/banking/api/controller/LoginServlet.java
+++ src/main/java/com/banking/api/controller/LoginServlet.java
@@ -18,7 +18,29 @@ public class LoginServlet extends HttpServlet {
 	public void init() throws  ServletException{
 		this.authservice = new AuthService();
 	}
-	
+	@Override
+	protected void doPost(HttpServletRequest req,HttpServletResponse resp )
+		throws ServletException,IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		User loggedInUser = authservice.authenticate(username, password);
+		
+		if(loggedInUser != null) {
+			HttpSession session = req.getSession(true);
+			//storing username and role for this session
+			session.setAttribute("username", loggedInUser.getusername());
+			session.setAttribute("role", loggedInUser.getrole());
+			
+			resp.setStatus(200); //OK
+			resp.getWriter().println("login success for" + loggedInUser.getrole() + loggedInUser.getusername());
+		}
+		else {
+			resp.setStatus(401); // unauthorized
+			resp.getWriter().println("invalid credentials");
+		}
+		
+	}
 	
 	
 }
